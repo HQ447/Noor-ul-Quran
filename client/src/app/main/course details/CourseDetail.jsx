cf69c04e13c1487f0225aea1f9c50e8e98586ae4
Hammad Ahmad <hamadqur447@gmail.com>
2025-08-08 11:52:36 +0500

    updated

--- client/src/app/main/course details/CourseDetail.jsx
+++ client/src/app/main/course details/CourseDetail.jsx
@@ -13,7 +13,7 @@ function CourseDetail() {
   const [course, setCourse] = useState(null);
   const [loading, setLoading] = useState(true);
   const { id } = useParams();
-  const BASE_URL = "http://localhost:8000";
+  const BASE_URL = "https://noor-ul-quran-backend-gq68.onrender.com";
   const [error, setError] = useState(null);
 
   // Simulate your existing logic
